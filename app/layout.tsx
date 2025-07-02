e19e324084776bb1ad7e10e43f04ad6fc63a08b6
Austinobravo <Austinobravo@gmail.com>
2025-07-02 12:24:54 +0100

    removed the style in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,13 +30,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* <meta charset="UTF-8"/> */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>My StyleXui Project</title>
-        {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous"></link> */}
-        {/* <link rel="stylesheet" href="https://cdn.stylexui.com/@v1.0.1-stable/css/xui.min.css"/> */}
-    </head>
       <body
         className={`${jost.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
